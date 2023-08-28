2a68f940d6790b83f556f4baba9665a3b8cf595b
hangyuCho <chg3376@gmail.com>
2023-08-29 08:47:53 +0900

    fix : draw bankground with white line thickness

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,42 @@
 
-var canvas: HTMLCanvasElement | null = document.querySelector("#playField")
-var image: HTMLImageElement | null = document.querySelector("#image")
-var context:CanvasRenderingContext2D | null | undefined = canvas?.getContext("2d")
-var imageSquareSize:number = 24
-var size:number = 40
-var framePerSecond:number = 24
-var gameSpeed:number = 5
+var canvas: HTMLCanvasElement | null 
+var image: HTMLImageElement | null
+var context:CanvasRenderingContext2D | null | undefined
+var gameMap: Array<Array<TempProp>>
+var gameOver: boolean
+var currentShape: Shape
+var nextShape: Shape
+var score: number
+var initialTwoDArr: Array<Array<TempProp>>
+var squareCountX:number
+var squareCountY:number
 
-class Block {
+const imageSquareSize:number = 24
+const size:number = 40
+const framePerSecond:number = 24
+const gameSpeed:number = 5
+const whiteLineThickness:number = 4
+
+interface TempProp {
+  imageX: number
+  imageY: number
+}
+
+interface InitialTwoDArrProp {
+  tempArray: TempProp[]
+}
+
+
+class Shape {
 
   imageX: number
   imageY: number
-  template: any[]
+  template: Array<Array<number>>
 
   constructor(
     imageX: number,
     imageY: number,
-    template: any[]
+    template: Array<Array<number>>
   ) {
     this.imageX = imageX
     this.imageY = imageY
@@ -35,7 +55,7 @@ class Block {
 }
 
 const shapes = [
-  new Block(0, 72, [
+  new Shape(0, 72, [
     [0, 1, 0],
     [0, 1, 0],
     [0, 1, 1]
@@ -54,27 +74,70 @@ const drawRect = (
   context.fillRect(x, y, width, height)
 }
 
-const gameLoop = () => {
-  setInterval(update, 1000 / gameSpeed)
-  setInterval(draw, 1000 / framePerSecond)
-}
-
 const update = () => {}
 
 const drawBackground = () => {
   if (!canvas) return
   drawRect(0, 0, canvas.width, canvas.height, "#bca0dc")
+  for (let i = 0; i < squareCountX + 1; i++) {
+    drawRect(size * i - whiteLineThickness, 0, whiteLineThickness, canvas.height, "white")
+  }
+  for (let i = 0; i < squareCountY + 1; i++) {
+    drawRect(0, size * i - whiteLineThickness, canvas.width , whiteLineThickness, "white")
+  }
 }
+
+const drawSquares = () => {}
+const drawCurrentShape = () => {}
+const drawNextShape = () => {}
+const drawGameOver = () => {}
 
 const draw = () => {
   if (!context || !canvas) return
   context.clearRect(0, 0, canvas.width, canvas.height)
   drawBackground()
+  drawSquares()
+  drawCurrentShape()
+  drawNextShape()
+  if (gameOver) {
+    drawGameOver()
+  }
+}
+
+
+const getRandomShape = (): Shape => {
+  return Object.create(shapes[Math.floor(Math.random() * shapes.length)])
+}
+
+const reestVars = () => {
+  initialTwoDArr = []
+  for (let i = 0; i < squareCountY; i++) {
+    var temp:TempProp[] = []
+    for (let j = 0; j < squareCountX; j++) {
+      temp.push({ imageX: -1, imageY: -1})
+    }
+    initialTwoDArr.push(temp)
+  }
+  score = 0
+  gameOver = false
+  currentShape = getRandomShape()
+  nextShape = getRandomShape()
+  gameMap = initialTwoDArr
 }
 
 window.onload = () => {
   canvas  = document.querySelector("#playField")
   image = document.querySelector("#image")
-  context = canvas?.getContext("2d")
+  if (canvas) {
+    context = canvas.getContext("2d")
+    squareCountX = canvas.width / size
+    squareCountY = canvas.height / size
+  }
 }
+
+const gameLoop = () => {
+  setInterval(update, 1000 / gameSpeed)
+  setInterval(draw, 1000 / framePerSecond)
+}
+
 gameLoop()
